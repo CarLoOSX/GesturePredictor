d425deb89e1a1e6d10d867287e9c6de3140722b7
Carlos Martinez <carloosx@MacBook-Pro.local>
2021-06-11 12:40:00 +0200

    api

---
+++ api/app.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_restplus import Api, Resource
+import pandas as pd
+import json
+from flask import request
+from flask import jsonify
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
+
+root_dir = os.path.abspath("")
+dir_plots = os.path.join(root_dir + "/api/", "plots")
+dir_model = os.path.join(root_dir + "/api/", "model")
+
+model_h5 = os.path.join(dir_model, "model.h5")
+model_weights = os.path.join(dir_model, "weights.h5")
+
+model = load_model(model_h5)
+model.load_weights(model_weights)
+
+# Keras Properties
+width, height = 150, 150
+
+flask_app = Flask(__name__)
+app = Api(app=flask_app)
+
+name_space = app.namespace('gesture-predictor', description='Gesture Predictor API')
+
+
+@name_space.route("/")
+class MainClass(Resource):
+    def post(self):
+        json_request = request.get_json(force=True)
+        data = pd.DataFrame(eval(json.dumps(json_request)))
+        plot = data.plot("timestamp")
+        fig = plot.get_figure()
+        file = dir_plots + "/" + "test" + ".png"
+        fig.savefig(file)
+        result = predict(file)
+        return jsonify({"result": result})
+
+
+def predict(file):
+    x = load_img(file, target_size=(width, height))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+
+    array = model.predict(x)
+    result = array[0]
+
+    answer = np.argmax(result)
+
+    value = ""
+    if np.max(result) > 0.8:
+        if answer == 0:
+            value = "MOVE_DOWN"
+        elif answer == 1:
+            value = "CIRCLE"
+        elif answer == 2:
+            value = "MOVE_LEFT"
+        elif answer == 3:
+            value = "MOVE_RIGHT"
+        elif answer == 4:
+            value = "MOVE_UP"
+    else:
+        value = "ERROR"
+    return value
